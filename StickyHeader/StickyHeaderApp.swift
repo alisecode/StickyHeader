5bb2aba7d79f1072c9be5e418b5468ca628f7129
Alisa <alisa.melissa@icloud.com>
2023-12-17 17:40:02 +0200

    Initial Commit

---
+++ StickyHeader/StickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderApp.swift
+//  StickyHeader
+//
+//  Created by Алиса Третьякова on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
