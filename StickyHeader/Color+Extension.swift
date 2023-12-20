41fea1fbf31783dc53c02b37458606ccb2c8d45f
Alisa <alisa.melissa@icloud.com>
2023-12-20 15:19:51 +0200

    new commit

---
+++ StickyHeader/Color+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extension.swift
+//  StickyHeader
+//
+//  Created by Алиса Третьякова on 17.12.2023.
+//
+
+import Foundation
---
+++ StickyHeader/MusicModel.swift
@@ -0,0 +1,8 @@
+//
+//  MusicModel.swift
+//  StickyHeader
+//
+//  Created by Алиса Третьякова on 17.12.2023.
+//
+
+import Foundation
